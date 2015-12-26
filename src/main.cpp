918768d033fbda0b9b9e25f9f705e53722f648e0
Casfire <admin@casfire.com>
2015-12-26 16:23:27 +0100

    SDL & glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
+#include "SDL/SDL.h"
+#include <exception>
+
 int main(int, char**) {
 	
-	return 0;
+	try {
+		
+		// TODO
+		
+		return 0;
+		
+	} catch (std::exception& e) {
+		
+		SDL_ShowSimpleMessageBox(
+			SDL_MESSAGEBOX_ERROR,
+			"Error", e.what(), nullptr
+		);
+		return 1;
+		
+	} catch (...) {
+		
+		SDL_ShowSimpleMessageBox(
+			SDL_MESSAGEBOX_ERROR,
+			"Error", "Unknown error", nullptr
+		);
+		return 1;
+		
+	}
 	
 }
